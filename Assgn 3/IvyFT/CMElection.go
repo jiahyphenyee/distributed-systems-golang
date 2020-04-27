2b0026d3e5d7497e69aa64b47842bd66264fb01d
Jia Yee <jiayee_koe@mymail.sutd.edu.sg>
2020-04-27 14:33:49 +0800

    fixed ticker

--- Assgn 3/IvyFT/CMElection.go
+++ Assgn 3/IvyFT/CMElection.go
@@ -5,107 +5,131 @@ import (
 	"time"
 )
 
-//Timeout ...
+//TimeoutDur ...
 const TimeoutDur time.Duration = 2 * time.Second
 
 // CM ...
 type CM struct {
-	ID       int
-	ReqList  []*Message
-	AllPages map[int]*PageInfo // pageID:PageInfo
-	Channel  chan Message
-	Pals     []*CM
-	Pri      int // ID of pri CM
-	Election bool
+	ID         int
+	ReqList    []*Message
+	AllPages   map[int]*PageInfo // pageID:PageInfo
+	Channel    chan Message
+	Pals       map[int]*CM
+	Pri        int // ID of pri CM
+	Election   bool
+	electReply map[int]int // replicas that ACK election
 }
 
 func initReplica(id int, CMPri int, page PageInfo) *CM {
 	cm := &CM{}
 	cm.ID = id
+	cm.AllPages = make(map[int]*PageInfo)
 	cm.AllPages[page.ID] = &page
 	cm.Channel = make(chan Message)
 	cm.Pri = CMPri
 	cm.Election = false
+	cm.Pals = make(map[int]*CM)
 	return cm
 }
 
 // Run ...
 func (cm *CM) Run(cms []*CM) {
-	cm.Pals = append(cm.Pals, cms...)
 
-	// get primary CM
+	// fill up Pals & get primary CM
 	var pri *CM
-	for j := 0; j < len(cm.Pals); j++ {
-		if cm.Pals[j].ID == cm.Pri {
-			pri = cm.Pals[j]
+	for j := 0; j < len(cms); j++ {
+		cm.Pals[cms[j].ID] = cms[j]
+		if cms[j].ID == cm.Pri {
+			pri = cms[j]
 		}
+
 	}
 
 	r := Request{
 		Requester: cm.ID,
-		ReqType:   PingCM,
+		ReqType:   Ping,
 		Channel:   make(chan (bool)),
 	}
 
 	pingMsg := Message{
 		Req:    r,
 		Sender: cm.ID,
-		Type:   PingCM,
+		Type:   Ping,
 	}
 
 	// periodic ping
 	ticker := time.NewTicker(5 * time.Second)
-	go func() {
-		for range ticker.C {
-			fmt.Printf("CM %d ping Pri", cm.ID)
+	// go func() {
+	// 	for range ticker.C {
+	// 		fmt.Printf("CM %d ping Pri", cm.ID)
 
-			timeout := make(chan bool, 1)
-			go func() {
-				time.Sleep(TimeoutDur)
-				timeout <- true
-			}()
+	// 		timeout := make(chan bool, 1)
+	// 		go func() {
+	// 			time.Sleep(TimeoutDur)
+	// 			timeout <- true
+	// 		}()
 
-			select {
-			case pri.Channel <- pingMsg:
-				return
-			case <-timeout:
-				// start election
-				timeoutMsg := Message{
-					Sender: cm.Pri,
-					Type:   Timeout,
-				}
-
-				cm.send(timeoutMsg, cm)
-
-			}
-		}
-	}()
+	// 		select {
+	// 		case pri.Channel <- pingMsg:
+	// 			return
+	// 		case <-timeout:
+	// 			// start election
+	// 			timeoutMsg := Message{
+	// 				Sender: cm.Pri,
+	// 				Type:   Timeout,
+	// 			}
+	// 			cm.send(timeoutMsg, cm)
+	// 		}
+	// 	}
+	// }()
 
 	// set up channels
 	for {
 		select {
 		case msg := <-cm.Channel:
 			cm.listen(msg)
+
+		case <-ticker.C:
+			if cm.ID != cm.Pri {
+				timeout := make(chan bool, 1)
+				go func() {
+					time.Sleep(TimeoutDur)
+					timeout <- true
+				}()
+
+				select {
+				case pri.Channel <- pingMsg:
+					continue
+				case <-timeout:
+					// start election
+					timeoutMsg := Message{
+						Sender: cm.Pri,
+						Type:   Timeout,
+					}
+					cm.send(timeoutMsg, cm)
+				}
+			}
+
 		default:
 			continue
 		}
 	}
-
 }
 
+// handle messages received
 func (cm *CM) listen(msg Message) {
-	fmt.Println("CM", cm.ID, "received", msg.Type, "from CM", msg.Sender)
+	fmt.Println(">>>>>CM", cm.ID, "received", msg.Type, "from CM", msg.Sender)
 
 	switch msg.Type {
-	case PingCM:
+	case Ping:
 		// send request list
-		broadcastMsg := Message{
+		consistencyMsg := Message{
 			Req:     msg.Req,
 			Sender:  cm.ID,
 			Type:    ReplyPing,
 			Content: cm.ReqList,
 		}
-		cm.send(broadcastMsg, cm.Pals[msg.Sender])
+		cm.send(consistencyMsg, cm.Pals[msg.Sender])
 
 	case ReplyPing:
 		cm.ReqList = msg.Content
@@ -130,9 +154,9 @@ func (cm *CM) listen(msg Message) {
 			cm.checkElectResult(msg.Sender)
 		case false:
 			// is the faulty node a coordinator
-			if cm.Coord == msg.Sender {
-				fmt.Println("Replica", cm.ID, "detects Pri CM", msg.Sender, "Timed out!"))
-				n.startElection()
+			if cm.Pri == msg.Sender {
+				fmt.Println("Replica", cm.ID, "detects Pri CM", msg.Sender, "Timed out!")
+				cm.startElection()
 			} else {
 				panic(fmt.Sprintln("who the heck timeout me"))
 			}
@@ -141,7 +165,17 @@ func (cm *CM) listen(msg Message) {
 	case Reject:
 		// stop election
 		cm.Election = false
+		cm.electReply = make(map[int]int)
 
+	case PriCM:
+		// announcement of new Pri CM
+		cm.Pri = msg.Sender
+
+		if cm.Election {
+			fmt.Println("Replica", cm.ID, "stops its election")
+			cm.Election = false
+			cm.electReply = make(map[int]int)
+		}
 	}
 }
 
@@ -156,40 +190,56 @@ func (cm *CM) send(msg Message, dest *CM) {
 // start election
 func (cm *CM) startElection() {
 	fmt.Println("CM", cm.ID, "started an election")
+	cm.electReply = make(map[int]int)
 	cm.Election = true
 	highest := true
 
-	electMsg := Message {
+	electMsg := Message{
 		Sender: cm.ID,
-		Type: Elect,
+		Type:   Elect,
 	}
 
 	for _, pal := range cm.Pals {
 		if pal.ID > cm.ID {
-			n.electReply[node] = node
+			cm.electReply[pal.ID] = pal.ID
 			highest = false
-			go n.send(msg, node)
+			go cm.send(electMsg, pal)
 		}
 	}
 
-}
-
-func (n *Node) startElection() {
-	fmt.Println("Node", n.ID, "started an election")
-	n.Election = true
-	msg := Message{"Elect", n}
-	highest := true
-
-	for _, node := range n.Pals {
-		if node.ID > n.ID {
-			n.electReply[node] = node
-			highest = false
-			go n.send(msg, node)
-		}
-	}
 	// if n is the highest ID, wins election
 	if highest {
-		n.broadcastWinner()
+		cm.broadcastWinner()
 		return
+	}
+}
+
+// announce the new Pri CM and reset election state
+func (cm *CM) broadcastWinner() {
+	broadcastMsg := Message{
+		Sender: cm.ID,
+		Type:   PriCM,
+	}
+
+	cm.Election = false
+	cm.Pri = cm.ID
+	fmt.Println("Replica", cm.ID, "is now the new Pri CM")
+
+	for _, pal := range cm.Pals {
+		go cm.send(broadcastMsg, pal)
+	}
+}
+
+// check if all requested nodes have replied to election request
+func (cm *CM) checkElectResult(palID int) {
+
+	// delete people who timed out my election msg
+	if _, found := cm.electReply[palID]; found {
+		delete(cm.electReply, palID)
+	}
+
+	// if all election requests timeout, wins election
+	if len(cm.electReply) == 0 {
+		cm.broadcastWinner()
 	}
 }
